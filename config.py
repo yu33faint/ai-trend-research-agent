71089d26f030c5cff5f7980666c67c4ffde83a79
yu33faint <youtailangsongwei2@gmail.com>
2026-06-27 12:23:41 +0900

    RSS取得設定をconfigに分離

---
+++ config.py
@@ -0,0 +1,13 @@
+RSS_SOURCES = [
+    {
+        "name": "Hacker News RSS",
+        "url": "https://hnrss.org/newest?q=AI",
+    },
+    {
+        "name": "OpenAI Blog",
+        "url": "https://openai.com/blog/rss.xml",
+    },
+]
+
+MAX_ARTICLES = 10
+ARTICLES_PER_SOURCE = 5
--- fetch_rss.py
+++ fetch_rss.py
@@ -1,18 +1,7 @@
 import feedparser
 
-RSS_SOURCES = [
-    {
-        "name": "Hacker News RSS",
-        "url": "https://hnrss.org/newest?q=AI",
-    },
-    {
-        "name": "OpenAI Blog",
-        "url": "https://openai.com/blog/rss.xml",
-    },
-]
+from config import ARTICLES_PER_SOURCE, MAX_ARTICLES, RSS_SOURCES
 
-MAX_ARTICLES = 10
-ARTICLES_PER_SOURCE = 5
 
 def fetch_articles():
     articles = []
