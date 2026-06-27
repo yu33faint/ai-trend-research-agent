95e4e7adf93f1337fed9c939331686173c57a5c2
yu33faint <youtailangsongwei2@gmail.com>
2026-06-27 14:57:55 +0900

    要約処理の入口を追加

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fetch_rss import fetch_articles
 from report_builder import build_markdown_report
 from report_writer import save_report
+from summarizer import summarize_article
 
 def main():
   articles = fetch_articles()
+
+  if articles:
+    articles[0]["summary"] = summarize_article(articles[0])
+
   report = build_markdown_report(articles)
   report_path = save_report(report)
 
---
+++ summarizer.py
@@ -0,0 +1,2 @@
+def summarize_article(article):
+  return f"{article['title']}に関する要約をここに生成します。"
